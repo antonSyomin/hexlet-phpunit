250a44a23045907e005e319eeead5945abc9f55e
Anton <anton.syomin@skillbox.ru>
2024-07-05 11:32:04 +0300

    fixed linter issue

--- src/Utils.php
+++ src/Utils.php
@@ -7,5 +7,3 @@ function reverseString($string)
     return implode(array_reverse(str_split($string)));
 }
 
-var_dump(array_values([2, 1, 2, 3]));
-var_dump(array_values(array_diff([2, 1, 2, 3], [12])));
