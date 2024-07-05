ce04df7e2344194cd2b8b87611f5b20ec410696e
Anton <anton.syomin@skillbox.ru>
2024-07-05 11:19:54 +0300

    xdebug

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -9,13 +9,8 @@ use function Hexlet\Phpunit\Utils\reverseString;
 // Имя класса совпадает с именем файла
 class UtilsTest extends TestCase
 {
-    // Метод (функция), определенный внутри класса,
-    // Должен начинаться со слова test
-    // Ключевое слово public нужно, чтобы PHPUnit мог вызвать этот тест снаружи
     public function testReverse(): void
     {
-        // Сначала идет ожидаемое значение (expected)
-        // И только потом актуальное (actual)
         $this->assertEquals('', reverseString(''));
         $this->assertEquals('olleh', reverseString('hello'));
     }
